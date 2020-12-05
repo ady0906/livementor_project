cfaaef896a32c6bdaecb9e548a63fd41402aa296
ady0906 <adrien.peynichou@gmail.com>
2020-12-05 20:01:26 +0100

    implemented functional converter

--- jsontocsv.rb
+++ jsontocsv.rb
@@ -2,19 +2,38 @@ require 'csv'
 require 'json'
 require 'net/http'
 
-def recursive_search(value)
-	puts value
-	if value.class == Integer || value.class == String
-		return value
+$final = Array.new
+$headers = Array.new
+
+def recursive_search(column, value)
+	if value.class == Hash
+		value.each do |key, element|
+			new_column = column + '.' + key
+			recursive_search(new_column, element)
+		end
+	else
+		$headers.append(column)
 	end
 end
 
 def make_headers(row)
-	final = Array.new
 	row.keys.each do |column|
-		final.append(recursive_search(row[column.to_s]))
+		recursive_search(column, row[column])
 	end
-	puts final
+	return $headers
+end
+
+def make_rows(headers, row)
+	values = Array.new
+	headers.each do |header|
+		value = header.split('.').inject(row,:[])
+		if value.class == Array
+			values.append(value.join(','))
+		else
+			values.append(value)
+		end
+	end
+	return values
 end
 
 ARGV.each do|url|
@@ -22,7 +41,16 @@ ARGV.each do|url|
 	json_response = JSON.parse(Net::HTTP.get(uri))
 	json_response.each_with_index do |row, index|
 		if index == 0
-			make_headers(row)
+			$final.append(make_headers(row))
+			$final.append(make_rows($headers, row))
+		else
+			$final.append(make_rows($headers, row))
 		end
 	end
+	csv_string = CSV.generate do |csv|
+		$final.each do |row|
+			csv << row
+		end
+	end
+	puts csv_string 
 end
